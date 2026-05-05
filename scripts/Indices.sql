e4b75e5aad00ddb8862da8c3b1b56708f00522c9
Ivo Giuliano Cappetto <ivogiuliano@hotmail.com>
2026-05-05 04:16:26 -0300

    Add files via upload

---
+++ scripts/Indices.sql
@@ -0,0 +1,8 @@
+
+CREATE INDEX idx_publicacion_id_usuario ON publicacion(id_usuario);
+
+CREATE INDEX idx_usuario_correo_hash ON usuario USING HASH (correo);
+
+CREATE INDEX idx_publicacion_etiquetas_gin ON publicacion USING GIN (etiquetas);
+
+CREATE INDEX idx_reporte_ventana_gist ON reporte_publicacion USING GIST (ventana_revision);
